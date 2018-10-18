78e6df68c9828e94c8dbfc3415fbbaff9effcbed
13606625315 <411533714@qq.com>
2018-10-18 15:41:54 +0800

    tcp socket!

---
+++ top/tcp/client.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/ip.h>
+#include<unistd.h>
+#include <arpa/inet.h>
+const int PORT = 6666;
+const int LISTENQ = 30;
+#define check(a) if(a<0) { printf("error = %d\n",__LINE__); exit(1);}
+const int MAXSIZE = 128;
+int main()
+{
+	struct sockaddr_in clientaddr;
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	clientaddr.sin_port = htons(PORT);
+	
+	int connetfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	connect(connetfd, (struct sockaddr* )&clientaddr, sizeof(clientaddr));
+	
+	char writebuf[MAXSIZE], readbuf[MAXSIZE+1];
+	while(fgets(writebuf, MAXSIZE, stdin) > 0)
+	{
+		write(connetfd, writebuf, strlen(writebuf));
+		
+		int n = read(connetfd,readbuf, MAXSIZE);
+		
+		readbuf[n] = 0;
+		
+		printf("client = %s\n",readbuf);
+	}
+	
+	
+}
---
+++ top/tcp/server.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/ip.h>
+#include<unistd.h>
+const int PORT = 6666;
+const int LISTENQ = 30;
+#define check(a) if(a<0) { printf("error = %d\n",__LINE__); exit(1);}
+const int MAXSIZE = 128;
+
+int main()
+{
+	struct sockaddr_in serveraddr;
+	bzero(&serveraddr,sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(PORT);
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int sockfd;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	check(sockfd);
+	
+	int ret = bind(sockfd,(struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	check(ret);
+	
+	ret = listen(sockfd, LISTENQ);
+	check(ret);
+	
+	struct sockaddr_in clientaddr;
+	for(;;)
+	{
+		socklen_t clientlen = sizeof(clientaddr);
+		int connetfd = accept(sockfd, (struct sockaddr*)&clientaddr, &clientlen);
+		check(connetfd);
+		if(fork() == 0)
+		{
+			close(sockfd);
+			ssize_t n = 0;
+			char buf[MAXSIZE];
+			while((n = read(connetfd, buf, MAXSIZE)) > 0)
+			{
+				printf("read = %s\n",buf);
+				write(connetfd, buf, n);
+			}
+			exit(0);
+		}
+		close(connetfd);
+	}
+	
+}
